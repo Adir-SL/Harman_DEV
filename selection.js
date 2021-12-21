12d0e568847d833e91892bf2dfcc19a03f9e53ec
Adir <slutzkis@gmail.com>
2021-12-21 19:19:30 +0200

    Uploaded files

---
+++ selection.js
@@ -0,0 +1,76 @@
+function selectFunc(){
+    document.getElementById("cardDiv").querySelector("video").src = "images/card.webm";
+    document.getElementById("cardFlex").style.pointerEvents = "all";
+    document.getElementById("cardFlex").style.opacity = "1";
+    document.getElementById("cardDiv").style.backdropFilter = "blur(15px)";
+
+    document.getElementsByClassName("prevBTN")[0].style.opacity = "0";
+    document.getElementsByClassName("nextBTN")[0].style.opacity = "1";
+
+    if(document.getElementById("controlSlider").value == "8"){
+        document.getElementById("contentImg").style.backgroundImage = "url(images/campaign_01-min.png)";
+        document.getElementById("contentImg").style.opacity = "1";
+
+        nextCount = 3;
+    }
+}
+
+function resetFunc(){
+    document.getElementById("hovers").style.backgroundImage = "url(images/hover_00-min.png)";
+    document.getElementById("hovers").querySelector("video").src = "";
+    document.querySelector(".dots").style.transform = "rotate(-55deg)";
+}
+
+function closeCard(){
+    document.getElementById("cardFlex").style.transform = "translateY(200%)";
+    document.getElementById("contentImg").style.opacity = "0";
+    
+
+    setTimeout(function(){
+        document.getElementById("cardDiv").style.backdropFilter = "blur(0)";
+        document.getElementById("cardFlex").style.pointerEvents = "none";
+        document.getElementById("cardFlex").style.opacity = "0";
+    }, 500);
+    setTimeout(function(){
+        document.getElementById("cardFlex").style.transform = "";
+        document.getElementById("contentImg").style.backgroundImage = "";
+    }, 1000);
+}
+
+function prevCard(){
+    tempCount = document.getElementById("contentImg").style.backgroundImage.slice(-11, -10);
+    tempStart = document.getElementById("contentImg").style.backgroundImage.slice(0, -11);
+    tempCount = eval(tempCount) - 1;
+
+    if(tempCount > 0){
+        document.getElementById("contentImg").style.backgroundImage = tempStart + tempCount + "-min.png";
+    }
+
+    checkBTNs();
+}
+
+function nextCard(){
+    tempCount = document.getElementById("contentImg").style.backgroundImage.slice(-11, -10);
+    tempStart = document.getElementById("contentImg").style.backgroundImage.slice(0, -11);
+    tempCount = eval(tempCount) + 1;
+
+    if(tempCount <= nextCount){
+        document.getElementById("contentImg").style.backgroundImage = tempStart + tempCount + "-min.png";
+    }
+
+    checkBTNs();
+}
+
+function checkBTNs(){
+    if(tempCount == 1){
+        document.getElementsByClassName("prevBTN")[0].style.opacity = "0";
+    }else{
+        document.getElementsByClassName("prevBTN")[0].style.opacity = "1";
+    }
+
+    if(tempCount == nextCount){
+        document.getElementsByClassName("nextBTN")[0].style.opacity = "0";
+    }else{
+        document.getElementsByClassName("nextBTN")[0].style.opacity = "1";
+    }
+}
---
+++ slider.js
@@ -0,0 +1,26 @@
+function sliderFunc(){
+    document.getElementById("controlDot").style.transform = "rotate("+document.getElementById("controlSlider").value * 30 + "deg)";
+    resetAllHovers();
+    selectHover();
+}
+
+function resetAllHovers(){
+    var x = document.getElementsByClassName("bg_hover");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.backgroundImage = "";
+    }
+}
+
+function selectHover(){
+    temp = eval(document.getElementById("controlSlider").value) + 1;
+    document.getElementById("hovers").style.backgroundImage = "url(images/hover_0" + temp + "-min.png)";
+    if(document.getElementById("controlSlider").value == 6 || document.getElementById("controlSlider").value == 0 || document.getElementById("controlSlider").value == 1 || document.getElementById("controlSlider").value == 2){
+        videoTemp = "images/connections_02.webm";
+    }else{
+        videoTemp = "images/connections_01.webm";
+    }
+    if(document.getElementById("hovers").querySelector("video").src.slice(-26) !== videoTemp){
+        document.getElementById("hovers").querySelector("video").src = videoTemp;
+    }
+}
